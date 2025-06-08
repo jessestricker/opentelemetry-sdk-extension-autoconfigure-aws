d3f993bfb152f7b62c4663eb1c5f2b7fda0e750e
Jesse Stricker <git@jessestricker.de>
2025-06-08 18:06:17 +0200

    feat: add AutoConfigurationCustomizerProvider implementation stub

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ repositories {
 }
 
 dependencies {
+    compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi:1.51.0")
+
     testImplementation("org.junit.jupiter:junit-jupiter:5.13.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ src/main/java/de/jessestricker/opentelemetry/sdk/extension/autoconfigure/aws/AwsAutoConfigurationCustomizerProvider.java
@@ -0,0 +1,11 @@
+package de.jessestricker.opentelemetry.sdk.extension.autoconfigure.aws;
+
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+
+public class AwsAutoConfigurationCustomizerProvider implements AutoConfigurationCustomizerProvider {
+  @Override
+  public void customize(AutoConfigurationCustomizer autoConfiguration) {
+    // TODO
+  }
+}
